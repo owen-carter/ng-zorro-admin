bcd9e546db83f5a9e6ad34d4d5367b260382a51c
owen-carter <owenjiao@outlook.com>
2017-08-21 18:56:28 +0800

    dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {
+    Component,
+    EventEmitter,
+    OnInit,
+    Input,
+    Output
+} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+    title = 'app';
+    isCollapsed = false;
+    _dataSet = [];
+
+    constructor() {
+    }
+
+    ngOnInit() {
+        for (let i = 0; i < 46; i++) {
+            this._dataSet.push({
+                key: i,
+                name: `Edward King ${i}`,
+                age: 32,
+                address: `London, Park Lane no. ${i}`,
+            });
+        }
+    }
+
+    nzCollapseChange() {
+        console.warn('2323')
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {NgZorroAntdModule} from 'ng-zorro-antd';
+import {AppComponent} from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        BrowserAnimationsModule,
+        NgZorroAntdModule.forRoot()
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
