79dd88afa6779884a833aaab303b40a65617e037
owen-carter <owenjiao@outlook.com>
2017-08-22 10:40:23 +0800

    init the bone

--- src/app/page/user/user.component.ts
+++ src/app/page/user/user.component.ts
@@ -16,17 +16,22 @@ export class UserComponent implements OnInit {
     userList = [];
 
     constructor() {
+
     }
 
     ngOnInit() {
         for (let i = 0; i < 46; i++) {
             this.userList.push({
-                key: i,
-                name: `Edward King ${i}`,
-                age: 32,
-                address: `London, Park Lane no. ${i}`,
+                'key': i,
+                'name': `Edward King ${i}`,
+                'age': 32,
+                'address': `London, Park Lane no. ${i}`,
             });
         }
     }
+
+    // trackByGrocery(index: number, grocery: userList): number {
+    //     return grocery.id;
+    // }
 
 }
