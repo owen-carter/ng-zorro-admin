723bc796c6727c82e38715df8a9ee5ccff7bacb3
owen-carter <owenjiao@outlook.com>
2017-08-26 21:46:37 +0800

    update

---
+++ src/app/page/track/track.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nz-track',
+  templateUrl: './track.component.html',
+  styleUrls: ['./track.component.less']
+})
+export class TrackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
