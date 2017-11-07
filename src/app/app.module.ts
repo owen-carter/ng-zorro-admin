3cd3177fe2ede9b2615447406fac17dce4077078
owen-carter <owenjiao@outlook.com>
2017-11-07 14:55:13 +0800

    fix the menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,22 +27,10 @@ import {LicenseComponent} from "./page/license/license.component";
 import {LogComponent} from "./page/log/log.component";
 import {AuditComponent} from "./page/audit/audit.component";
 import {SystemComponent} from "./page/system/system.component";
-import {SearchComponent} from "./page/search/search.component";
-import {CustomerComponent} from "./page/customer/customer.component";
-import {CreateComponent} from "./page/create/create.component";
-import {TrackComponent} from "./page/track/track.component";
-import {ManageComponent} from "./page/manage/manage.component";
-import {TestComponent} from "./page/test/test.component";
-// feature
 // pipe
 import {TimestampPipe} from "./pipe/timestamp.pipe";
 import {UserEditComponent} from "./page/user/children/user-edit/user-edit.component";
-import {ZoomDirective} from "./directive/zoom.directive";
-import {ProjectComponent} from "./page/project/project.component";
-import {ProjectImportComponent} from "./page/project/children/project-import/project-import.component";
-import {ProjectEditComponent} from "./page/project/children/project-edit/project-edit.component";
 import {RoleEditComponent} from "./page/role/children/role-edit/role-edit.component";
-import {CustomerEditComponent} from "./page/customer/children/customer-edit/customer-edit.component";
 import {RemoteComponent} from './page/remote/remote.component';
 
 @NgModule({
@@ -64,21 +52,10 @@ import {RemoteComponent} from './page/remote/remote.component';
         LogComponent,
         AuditComponent,
         SystemComponent,
-        SearchComponent,
-        CustomerComponent,
         TimestampPipe,
-        CreateComponent,
-        TrackComponent,
-        ManageComponent,
-        TestComponent,
         UserEditComponent,
-        ZoomDirective,
-        ProjectComponent,
-        ProjectImportComponent,
-        ProjectEditComponent,
         RoleEditComponent,
-        CustomerEditComponent,
-        RemoteComponent,
+        RemoteComponent
     ],
     imports: [
         BrowserModule,
@@ -94,4 +71,5 @@ import {RemoteComponent} from './page/remote/remote.component';
 })
 export class AppModule {
 }
+
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,10 @@
 import {Routes} from '@angular/router';
 // Component
 import {DashboardComponent} from './page/dashboard/dashboard.component';
-
 import {UserComponent} from './page/user/user.component';
 import {UserEditComponent} from './page/user/children/user-edit/user-edit.component';
-
 import {RoleComponent} from './page/role/role.component';
 import {PermissionComponent} from './page/permission/permission.component';
-
 import {ConfigureComponent} from './page/configure/configure.component';
 import {SettingComponent} from './page/setting/setting.component';
 import {UpgradeComponent} from './page/upgrade/upgrade.component';
@@ -19,29 +16,16 @@ import {LicenseComponent} from './page/license/license.component';
 import {LogComponent} from './page/log/log.component';
 import {AuditComponent} from './page/audit/audit.component';
 import {RemoteComponent} from './page/remote/remote.component';
-
 import {SystemComponent} from './page/system/system.component';
-import {SearchComponent} from './page/search/search.component';
 
-import {CustomerComponent} from './page/customer/customer.component';
-import {CreateComponent} from './page/create/create.component';
-import {TrackComponent} from './page/track/track.component';
-import {ManageComponent} from './page/manage/manage.component';
-import {TestComponent} from './page/test/test.component';
-
-import {ProjectImportComponent} from './page/project/children/project-import/project-import.component';
 
 export const routes: Routes = [
     {path: '', component: DashboardComponent},
     {path: 'dashboard', component: DashboardComponent},
-
     {path: 'user', component: UserComponent},
     {path: 'user.edit/:id', component: UserEditComponent},
-
     {path: 'role', component: RoleComponent},
-
     {path: 'permission', component: PermissionComponent},
-
     {path: 'setting', component: SettingComponent},
     {path: 'upgrade', component: UpgradeComponent},
     {path: 'backup', component: BackupComponent},
@@ -50,16 +34,6 @@ export const routes: Routes = [
     {path: 'remote', component: RemoteComponent},
     {path: 'audit', component: AuditComponent},
     {path: 'system', component: SystemComponent},
-    {path: 'search', component: SearchComponent},
-    {path: 'customer', component: CustomerComponent},
-
-    {path: 'project.create', component: CreateComponent},
-    {path: 'project.import', component: ProjectImportComponent},
-    {path: 'project.track', component: TrackComponent},
-    {path: 'project.manage', component: ManageComponent},
-
     {path: 'configure', component: ConfigureComponent},
-    {path: 'test', component: TestComponent}
-
-    // { path: '**', component: PageNotFoundComponent }
+    {path: '**', component: DashboardComponent}
 ];
